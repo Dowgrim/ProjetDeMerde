943bd5d1402334fae0aa0fc765e906b5eb3055ad
Florent Goumot-Labesse <yozamu@hotmail.fr>
2015-06-10 09:55:02 +0200

    => TODO LIST dans un fichier séparé

---
+++ Cache_Project/TodoList.c
@@ -0,0 +1,13 @@
+/*
+TODO LIST :
+
+- Corriger cache_list pour pouvoir compiler
+- Tester stratégies NUR
+- Voir résultats LRU. "Choisir bloc aléatoire" ?
+- Cache_Sync dans cache.c, ajouter l'écriture dans le fichier
+- Cache.c, fonctions read,write,getInstrument(pour ce dernier lire rq début page 10)
+- low_cache.c à faire
+- Retirer l'utilisation de la librairie fournie
+- Analyse des résultats
+- Etendre l'instrumentation du cache ? Ajouter des tests ?
+*/
