d575e389ffe0e2085260327321b3b2f975c8916a
Florent Goumot-Labesse <yozamu@hotmail.fr>
2015-06-10 09:57:19 +0200

    FUCK THIS SHIT

--- Cache_Project/TodoList.c
+++ Cache_Project/TodoList.c
@@ -2,10 +2,12 @@
 TODO LIST :
 
 - Corriger cache_list pour pouvoir compiler
-- Tester stratégies NUR
+- Tester stratégie NUR
 - Voir résultats LRU. "Choisir bloc aléatoire" ?
 - Cache_Sync dans cache.c, ajouter l'écriture dans le fichier
-- Cache.c, fonctions read,write,getInstrument(pour ce dernier lire rq début page 10)
+- Cache.c fonction read
+- Cache.c fonction write
+- Cache.c fonction getInstrument(lire remarque début page 10)
 - low_cache.c à faire
 - Retirer l'utilisation de la librairie fournie
 - Analyse des résultats
