5cdbb2cde97fb7d45031b272916c08763291e2c0
Florent Goumot-Labesse <yozamu@hotmail.fr>
2015-06-10 15:34:56 +0200

    /!\ Ajout strat NUR, modif LOW_CACHE.H /!\

--- Cache_Project/NUR_strategy.c
+++ Cache_Project/NUR_strategy.c
@@ -4,10 +4,26 @@
 #include "low_cache.h"
 #include "time.h"
 
+
+void deref(struct Cache *pcache)
+{
+    // on met le bit REFER à 0 dans tous les blocs
+    for (int i = 0; i < pcache->nblocks; i++)
+        pcache->headers[i].flags &= ~REFER;
+    // ràz du compteur
+    pcache->pstrategy = 0;
+    // on incrémente le compteur
+    ++pcache->instrument.n_deref;
+}
+
+// calcul de la valeur de rm : 2 * R + M
+int valuate_rm(struct Cache_Block_Header *bloc) {
+    int rm = 0;
+    return rm = (2 * (bloc->flags & REFER)) + (bloc->flags & MODIF);
+}
+
 void *Strategy_Create(struct Cache *pcache) 
 {
-    // srand((unsigned int)time(NULL));
-    return NULL;
 }
 
 void Strategy_Close(struct Cache *pcache)
@@ -16,47 +32,52 @@ void Strategy_Close(struct Cache *pcache)
 
 void Strategy_Invalidate(struct Cache *pcache)
 {
+    deref(pcache);
 }
 
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
 {
-    int ib, min;
+    // initialisation du min à la valeur max du flag (une valeur qui ne peut être atteinte)
+    int min = 4;
     struct Cache_Block_Header *pbh;
+    // on regardera la valeur du flag le plus petit
     struct Cache_Block_Header *pbh_min = NULL;
 
     /* On cherche d'abord un bloc invalide */
     if ((pbh = Get_Free_Block(pcache)) != NULL)
         return pbh;
-    // initialisation du min à la valeur max du flag
-    min = 4;
-    for (ib = 0; ib < pcache->nblocks; ib++) {
-        pbh = &pcache->headers[ib];
+    for (int i = 0; i < pcache->nblocks; i++) {
+        pbh = &pcache->headers[i];
         // si le flag est nul, on a trouvé le bon bloc
-        if (pbh->flags == 0) {
+        if (valuate_rm(pbh) == 0) {
             return pbh;
         }
         // sinon on cherche la valeur du flag minimale
-        else if (pbh->flags < min) {
+        else if (min == 4 || valuate_rm(pbh) < min) {
             min = pbh->flags;
             pbh_min = pbh;
-        }
-        
+        }   
     }
-
     return pbh_min;
 }
 
-
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
 {
-    // on met le premier bit à 1
-    pbh->flags |= 1 << 1;
+    // réinitialisation du compteur si on dépasse la limite de nderef
+    // (on a fait ndref accès)
+    if (++pcache->pstrategy >= pcache->nderef)
+        deref(pcache);
+
+    // on met le bit REFER à 1 (on a accédé au bloc)
+    pbh->flags |= REFER;
 }  
 
 void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
 {
-    // on met le flag à la valeur maximale (R et M à 1)
-    pbh->flags = 3;
+    if ((++pcache->pstrategy) >= pcache->nderef)
+        deref(pcache);
+
+    pbh->flags |= REFER;
 } 
 
 char *Strategy_Name()
