2056d2a7cd3a5e68c88ec5f61e9cbcc9c7281ead
Florent Goumot-Labesse <yozamu@hotmail.fr>
2015-06-11 15:08:21 +0200

    MAJ todo list

--- Cache_Project/TodoList.c
+++ Cache_Project/TodoList.c
@@ -1,15 +1,7 @@
 /*
 TODO LIST :
 
-- Corriger cache_list pour pouvoir compiler (dé-commenter le fichier, virer lib)
-- Tester stratégie NUR
-- Voir résultats LRU. "Choisir bloc aléatoire" ?
-- Cache_Sync dans cache.c, ajouter l'écriture dans le fichier
-- Cache.c fonction read
 - Cache.c fonction write
-- Commenter ce qu'on a fait pour permettre aux autres de comprendre
-- Fonctions de cache.c sont censées renvoyer Cache_Error... Que mettre ?
 - Retirer l'utilisation de la librairie fournie
 - Analyse des résultats
-- Etendre l'instrumentation du cache ? Ajouter des tests ?
 */
