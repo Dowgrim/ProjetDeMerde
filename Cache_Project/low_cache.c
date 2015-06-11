e8f31ff3110f30843a176d627d135db8c37eb652
Florent Goumot-Labesse <yozamu@hotmail.fr>
2015-06-11 10:14:58 +0200

    Correction low_cache.c

--- Cache_Project/low_cache.c
+++ Cache_Project/low_cache.c
@@ -4,9 +4,9 @@ struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache) {
 	int i=0, j=pcache->nblocks;
 	for(i = 0; i < j; i++) {
 		// On parcoure les blocs du cache
-		if(pcache->headers[i].flags == 0) {
+		if(pcache->headers[i].flags &= ~VALID) {
 			// On a un bloc libre, on le retourne
-			return pcache->headers[i];
+			return &(pcache->headers[i]);
 		}
 	}
 	// On a pas trouvé de bloc libre, on return alors NULL comme indiqué dans le sujet
